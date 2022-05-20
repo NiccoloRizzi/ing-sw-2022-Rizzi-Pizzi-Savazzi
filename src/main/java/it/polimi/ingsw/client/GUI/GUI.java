79b0099475258763e1f1a6228e2f32daa903cb67
LordAlberico <federico3.pizzi@mail.polimi.it>
2022-05-20 18:38:19 +0200

    First GUI test

---
+++ src/main/java/it/polimi/ingsw/client/GUI/GUI.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.client.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/prova.fxml"));
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.show();
+    }
+}
